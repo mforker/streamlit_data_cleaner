03e410841d0c9947a2cf304e70bd9becc681bde6
Mitesh <nandanmitesh@gmail.com>
2024-03-17 18:02:49 +0530

    updated app

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.set_page_config(page_title="Data Cleaner",layout="wide")
 import pandas as pd
 
-st.header("Data Cleaner")
-st.subheader("This app cleans data using python.", divider="rainbow")
+st.header("Data Stats")
+st.subheader("This app describes data using python.", divider="rainbow")
 
 col1,col2 = st.columns([0.3,0.7])
 
@@ -30,17 +30,60 @@ with st.container():
 
         st.markdown('''-----''')
 
+        st.write(f'First 5 rows of {uploaded_file.name}')
+        st.dataframe(sample)
+
         col3,col4 = st.columns([0.4,0.6])
         
         with col3:
-            st.write(f'First 5 rows of {uploaded_file.name}')
-            st.dataframe(sample)
-        with col4:
             st.write(f'Data Summary')
-            st.write(df.describe())
-        st.write("NA Values in each Column:")
-        st.write(df.isna().sum())
-                    
+            st.dataframe(df.describe(), height=350)
+            
+        with col4:
+            st.write("NA Values in each Column:")
+            na_df = pd.DataFrame(df.isna().sum())
+            print(na_df.columns)
+            st.dataframe(na_df, height=350)
+
+        a = pd.DataFrame(
+            {
+                'columns name' : df.columns.to_list(), 
+                'default data types': df.dtypes,
+                'data types': None
+            }
+        )
+        col5, col6 = st.columns(2)
+        with col5:
+            st.write('Select Datatypes')
+            ed= st.data_editor(a, column_config={
+                'data types': st.column_config.SelectboxColumn(
+                    "select Data Type to convert",
+                    help="Select the data type of the column",
+                    width="medium",
+                    options = ['int','str','float','bool','object'],
+                    required= False,
+                    disabled=False
+                ),
+                'default data types' : st.column_config.Column(disabled=True),
+                'columns name': st.column_config.Column(disabled=True)
+            }, hide_index= True)
+        with col6:
+            st.markdown(f'''\n \n''')
+            if st.button('Convert Datatypes', use_container_width=True):
+                for col, def_typ, typ in zip(ed['columns name'],ed['default data types'],ed['data types']):
+                    if typ is not None:
+                        if typ in ['int','float']:
+                            try:
+                                df[col] = df[col].str.replace('$','').str.replace('₹','').str.replace(',','').str.replace(' ','').str.strip().astype(typ)
+                            except ValueError:
+                                st.warning(f'can not convert {col} to {typ} try converting to a valid type')
+                            # st.dataframe(df[col])                        
+                        else:
+                            df[col] = df[col].astype(typ)
+                
+                st.write(df.dtypes)  
+            else:
+                st.warning('comp')             
            
 
 
